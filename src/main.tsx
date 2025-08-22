339dc25df3a4eee4be5c6a2a350caaadd9a83aa3
Gano603 <khawar603@gmail.com>
2025-08-22 18:29:08 +0500

    TanStack Router and Tailwindcss Installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
+import { router } from './routes/routes.tsx'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from '@tanstack/react-router'
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
